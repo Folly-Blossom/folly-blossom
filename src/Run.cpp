d262446a793701fc9e6260801e9f312833b2022a
Christian Vega <christian.angel.vega00@gmail.com>
2024-05-05 20:06:26 -0400

    Setup data API

---
+++ src/Run.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "[*] Please specify a file name.";
+
+    const std::string FILE_NAME, FILE_PATH;
+
+    std::cin >> (std::string&) FILE_NAME;
+
+    std::cout << "[*] Please specify the appropiate file path.";
+
+    std::cin >> (std::string&) FILE_PATH;
+
+    if (FILE_NAME.empty() || FILE_PATH.empty()) {
+        std::cout << "[-] Either the given file name or path are null and cannot be used.";
+    }
+
+    return 0;
+}
